eda8999f436fe927161a9c7b8ade82a2a24e465c
Thilak238 <thilakchakravarthi1@gmail.com>
2020-08-10 22:47:43 +0530

    Updates

---
+++ precache-manifest.4c9026945fb208837519bccd114fc27a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d4b729d59837cb577f1c25242dd070",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6f61a578c8d18b72917c",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2c59fd741f1e4963b85f",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "6f61a578c8d18b72917c",
+    "url": "/contactmanager/static/js/2.37629bfb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.37629bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c59fd741f1e4963b85f",
+    "url": "/contactmanager/static/js/main.a366478c.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
